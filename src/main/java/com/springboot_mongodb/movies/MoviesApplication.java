e4c9e9de9c83ce5680f69a154e3967c45b1aee2a
Jashan Pahwa <jashan@Jashans-MacBook-Air.local>
2024-07-07 18:42:49 +0530

    Initial commit

---
+++ src/main/java/com/springboot_mongodb/movies/MoviesApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_mongodb.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+}
